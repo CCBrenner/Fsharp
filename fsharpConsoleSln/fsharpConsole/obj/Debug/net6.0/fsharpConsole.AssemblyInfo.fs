a3bb6b275da0e21ede0fa8f266911b3d2ac3bab3
Collyn Brenner <collynbrenner@gmail.com>
2024-01-08 15:12:39 -0600

    pause on fsharp stuff for a little while

--- fsharpConsoleSln/fsharpConsole/obj/Debug/net6.0/fsharpConsole.AssemblyInfo.fs
+++ fsharpConsoleSln/fsharpConsole/obj/Debug/net6.0/fsharpConsole.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("fsharpConsole")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96f99d6ae206cd6b1db0bb347710742babffb4b5")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("fsharpConsole")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("fsharpConsole")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
--- fsharpConsoleSln/fsharpConsoleTests/obj/Debug/net6.0/fsharpConsoleTests.AssemblyInfo.fs
+++ fsharpConsoleSln/fsharpConsoleTests/obj/Debug/net6.0/fsharpConsoleTests.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("fsharpConsoleTests")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96f99d6ae206cd6b1db0bb347710742babffb4b5")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("fsharpConsoleTests")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("fsharpConsoleTests")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
